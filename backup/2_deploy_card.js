c1ffda5166dfbae1977c4449461e99c4f308d276
hyosoonim <gytns0087@naver.com>
2019-09-11 11:14:30 +0900

    feat : implement HyoBank

---
+++ backup/2_deploy_card.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const AzureUploader = require('@flui/klaytn-uploader').AzureUploader;
+const HyoBank = artifacts.require('HyoBank');
+
+module.exports = function(deployer) {
+	deployer.deploy(HyoBank).then(() => {
+		const data = JSON.stringify({
+			contractAddress: HyoBank.address
+		});
+
+		if (!fs.existsSync('./artifacts')) {
+			fs.mkdirSync('./artifacts');
+			console.log(`\n    Create Artifacts`);
+		}
+
+		fs.writeFileSync('./artifacts/address.json', data);
+		console.log(`\n    Create file of contract address to json: ${HyoBank.address}`);
+
+		const abi = JSON.stringify(HyoBank._json.abi);
+		fs.writeFileSync('./artifacts/abi.json', abi);
+		console.log(`\n    Create file of abi file to json: ${HyoBank.address}`);
+
+		const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+		const accessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+		const uploader = new AzureUploader(accountName, accessKey);
+
+		const containerName = process.env.AZURE_STORAGE_CONTRACT_CONTAINER_NAME;
+		uploader.uploadArtifacts(HyoBank._json.contractName, 'artifacts', containerName);
+	});
+};
